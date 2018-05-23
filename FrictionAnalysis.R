bc966fa0756d3ca91a18e252a3377afba22fd863
githubmao <maoyanbit@foxmail.com>
2018-05-23 22:40:08 +0800

    不同MU，12%单坡；Mu3.5 & Mu4.5 uphill。

--- FrictionAnalysis.R
+++ FrictionAnalysis.R
@@ -59,3 +59,95 @@ plot.friction <- ggplot(df.friction,
 plot.friction
 
 
+# Vertical, Mu3.5 & Mu4.5----
+df.verticalMu3.5 <- fread(input = "E:/R/DongAo/Slope/VerticalFriction/coasterSpeed20_3.5.txt",
+                          header = TRUE,
+                          sep = "auto",
+                          stringsAsFactors = FALSE,
+                          data.table = FALSE,
+                          skip = "Station",
+                          col.names = c("Station", "Speed"))
+
+
+df.verticalMu4.5 <- fread(input = "E:/R/DongAo/Slope/VerticalFriction/coasterSpeed20_4.5.txt",
+                          header = TRUE,
+                          sep = "auto",
+                          stringsAsFactors = FALSE,
+                          data.table = FALSE,
+                          skip = "Station",
+                          col.names = c("Station", "Speed"))
+
+# 增加数据项 Mu----
+df.verticalMu3.5$Mu <- "3.5"
+df.verticalMu4.5$Mu <- "4.5"
+
+
+# 合并数据----
+df.verticalMu <- rbind(df.verticalMu3.5,
+                       df.verticalMu4.5)
+
+
+# Plot, Vertical, Mu 3.5----
+plot.coasterMu3.5 <- ggplot(df.verticalMu3.5,
+                            aes(x = Station, y = Speed)) +
+  geom_path(size = 1.2) +
+  geom_hline(yintercept = 20, linetype = "dashed", color = "red", size = 1.0) +
+  scale_x_continuous(limits = c(100, 7523),
+                     breaks = c(seq(100, 7100, 500), 7523),
+                     labels = c("K0+000", "K0+500", "K1+000", "K1+500",
+                                "K2+000", "K2+500", "K3+000", "K3+500",
+                                "K4+000", "K4+500", "K5+000", "K5+500",
+                                "K6+000", "K6+500", "K7+000", "K7+423")) +
+  scale_y_continuous(limits = c(0, 30), breaks = seq(0, 30, 5)) +
+  labs(x = "位置", y = "速度，km/h") +
+  theme(legend.title = element_text(face = "bold", size = 10),
+        legend.text = element_text(face = "bold", size = 10),
+        axis.title = element_text(face = "bold", size = 11),
+        axis.text = element_text(face = "bold", size = 10))
+
+plot.coasterMu3.5
+
+
+# Plot, Vertical, Mu 4.5----
+plot.coasterMu4.5 <- ggplot(df.verticalMu4.5,
+                            aes(x = Station, y = Speed)) +
+  geom_path(size = 1.2) +
+  geom_hline(yintercept = 20, linetype = "dashed", color = "red", size = 1.0) +
+  scale_x_continuous(limits = c(100, 7523),
+                     breaks = c(seq(100, 7100, 500), 7523),
+                     labels = c("K0+000", "K0+500", "K1+000", "K1+500",
+                                "K2+000", "K2+500", "K3+000", "K3+500",
+                                "K4+000", "K4+500", "K5+000", "K5+500",
+                                "K6+000", "K6+500", "K7+000", "K7+423")) +
+  scale_y_continuous(limits = c(0, 30), breaks = seq(0, 30, 5)) +
+  labs(x = "位置", y = "速度，km/h") +
+  theme(legend.title = element_text(face = "bold", size = 10),
+        legend.text = element_text(face = "bold", size = 10),
+        axis.title = element_text(face = "bold", size = 11),
+        axis.text = element_text(face = "bold", size = 10))
+
+plot.coasterMu4.5
+
+
+# Plot, Vertical, Mu----
+plot.coasterMu <- ggplot(df.verticalMu,
+                         aes(x = Station, y = Speed)) +
+  geom_path(aes(colour = factor(Mu)), size = 1.2) +
+  geom_hline(yintercept = 20, linetype = "dashed", color = "red", size = 1.0) +
+  scale_x_continuous(limits = c(100, 7523),
+                     breaks = c(seq(100, 7100, 500), 7523),
+                     labels = c("K0+000", "K0+500", "K1+000", "K1+500",
+                                "K2+000", "K2+500", "K3+000", "K3+500",
+                                "K4+000", "K4+500", "K5+000", "K5+500",
+                                "K6+000", "K6+500", "K7+000", "K7+423")) +
+  scale_y_continuous(limits = c(0, 30), breaks = seq(0, 30, 5)) +
+  labs(x = "位置", y = "速度，km/h") +
+  scale_colour_hue("Friction Mu")+
+  theme(legend.title = element_text(face = "bold", size = 10),
+        legend.text = element_text(face = "bold", size = 10),
+        axis.title = element_text(face = "bold", size = 11),
+        axis.text = element_text(face = "bold", size = 10))
+
+plot.coasterMu
+
+
